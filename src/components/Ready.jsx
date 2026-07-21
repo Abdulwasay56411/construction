e34a26fafd771575de60da4ad36c3e69ddc9240e
wasay <wasay56411@gmail.com>
2026-07-21 16:12:20 +0500

    update

--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -4,9 +4,9 @@ const Ready = () => {
   return (
     <div className='px-5 md:px-8 lg:px-10 py-10 bg-[#D48E26] h-116.5 flex flex-col justify-center items-center text-center'>
     <div>
-        <h1 className='font-bold text-[55px] leading-16 text-[#FFFFFF]'>Ready to Build Your <br />
+        <h1 className='font-bold text-xl sm:text-4xl lg:text-[55px] leading-8 sm:leading-12  lg:leading-16 text-[#FFFFFF]'>Ready to Build Your <br />
         Dream Team in Saudi Arabia?</h1>
-        <p className='text-[#FFFFFF] text-lg max-w-3xl py-2'>Join 100+ leading companies that trust Telal Al Shaheqa for their critical
+        <p className='text-[#FFFFFF] text-[10px] sm:text-lg max-w-3xl py-2'>Join 100+ leading companies that trust Telal Al Shaheqa for their critical
         workforce requirements. Fast, compliant, and professional.</p>
     </div>
     </div>
